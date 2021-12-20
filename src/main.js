2e4b9c09d85f74e251aea9e45442cb469cd9571b
vincentauzias <89008153+vincentauzias@users.noreply.github.com>
2021-12-20 11:26:27 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from './axios'
+import axios from 'axios'
 
 const app = createApp(App)
 app.use(router)
