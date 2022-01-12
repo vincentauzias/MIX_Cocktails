96b6bf537bfb9a8c642c6f4a0cffdb9912362fc1
RyuuCreator <87366929+RyuuCreator@users.noreply.github.com>
2022-01-12 12:11:42 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <Footers></Footers>
 </template>
 
 <script>
@@ -7,12 +7,26 @@
 import "normalize.css"
 import "@fortawesome/fontawesome-free/js/all.js"
 
+import Footers from './components/Footer.vue'
+
+
 export default {
-    
+  name: 'App',
+    components: {
+      Footers,
+    },
 }
 
 </script>
 
 <style>
-
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 100vh;
+    background-color: #FFFEF9;
+    overflow: hidden;
+  }  
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+    <footer>
+        <span>
+            Technologie utilisé: NodeJs, Vue3, MongoDB. Plus d'information sur GitHub
+            <a href="https://github.com/vincentauzias/MIX_Cocktails"><i class="fab fa-github"></i></a>
+        </span>
+        <hr class="footer-hr">
+        <span>Contributeur: Vincent Auzias - Mickael Tailhades - Emmanuelle Dufrenay</span>
+    </footer>
+</template>
+
+<script>
+
+    export default {
+        name: 'Footer'
+    }
+
+</script>
+
+<style scoped>
+    footer {
+        padding: 15px 0;
+        background-color: #82A7CA;
+        border-top: 1px solid #2974BB;
+        text-align: center;
+        vertical-align: middle;
+        width: 100%;
+    }
+
+    .footer-hr {
+        border: none;
+        background-color: #333;
+        width: 50%;
+        height: 1px;
+    }
+
+    .fa-github {
+        font-size: 1.25em;
+        color: #333;
+    }
+</style>
