74987dbf019c8763cee13a7bd1bfbbdcd53abf80
vincentauzias <89008153+vincentauzias@users.noreply.github.com>
2022-01-12 14:56:25 +0100

    test merge plein de trucs

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,6 +2,7 @@ import  express  from 'express'
 import  bodyParser  from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import nodemailer from "nodemailer";
 
 
 
@@ -35,5 +36,76 @@ app.get('/', (req, res) => {
 
     res.send('Hello ducon')
 })
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use("/public", express.static("/public"));
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+    },
+});
+
+transporter.verify(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Server is ready to take our message");
+    }
+});
+
+app.post("/api/send", (req, res) => {
+    console.log("hi");
+
+    let name = req.body.name;
+    let email = req.body.email;
+    let firstname = req.body.firstname;
+    // let birthDate = req.body.birthDate;
+    let subject = req.body.subject;
+    let message = req.body.message;
+
+    console.log(name);
+
+    const mail = {
+        from: name + " " + email,
+        to: process.env.EMAIL,
+        subject: subject,
+        html:
+            '<p style="font-size:20px; padding:2%">' +
+            message +
+            "</p><br>" +
+            '<span style="display:flex; justify-content:flex-end; font-size:18px; padding-right:3%">' +
+            firstname +
+            " " +
+            name +
+            "</span>",
+    };
+
+    transporter.sendMail(mail, (err) => {
+        if (err) {
+            console.log(err);
+            // res.status(500).send("Something went wrong."); éviter le status 500, peut faire cracher la page
+            res.status(200).json({
+                msg: "Une erreur est survenue, merci de rééssayer plus tard.",
+            });
+        } else {
+            // console.log("hilo");
+            res.status(200).json({
+                msg: "OK",
+            });
+        }
+    });
+});
+
+
 
 app.listen(PORT, () => console.log(`Serveur activé sur le port http://localhost:${PORT}`))
